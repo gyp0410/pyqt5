e0ccd433c84ffe29a8708639f9b2b93b337a187b
gyp0410 <1624329413@qq.com>
2020-04-11 14:04:37 +0800

    MainWindow

---
+++ 小窗口/QLineEditEchoMode.py
@@ -0,0 +1,65 @@
+'''
+
+QLineEdit控件与回显模式
+
+基本功能：输入单行的文本
+
+EchoMode(回显模式)
+
+4种回显模式
+
+1.Normal                正常显示
+2.NoEcho                不显示密码
+3.Password              不显示密码，显示黑点
+4.PasswordEchoOnEdit    刚输入显示，后面变成黑点
+
+
+
+'''
+
+
+from PyQt5.QtWidgets import *
+import sys
+
+class QLineEditEchoMode(QWidget):
+    def __init__(self):
+        super(QLineEditEchoMode,self).__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        self.setWindowTitle('文本输入框的回显模式')
+
+        formLayout = QFormLayout()          #创建表单布局
+
+        normalLineEdit = QLineEdit()
+        noEchoLineEdit = QLineEdit()
+        passwordLineEdit = QLineEdit()
+        passwordEchoOnEditLineEdit = QLineEdit()
+
+        formLayout.addRow("Nomal",normalLineEdit)       #表单布局
+        formLayout.addRow("NoEcho",noEchoLineEdit)
+        formLayout.addRow("Password",passwordLineEdit)
+        formLayout.addRow("PasswordEchoOnEdit",passwordEchoOnEditLineEdit)
+
+        #placeholdertext  在文本什么都没有输入的时候显示灰色的字体提示
+
+        normalLineEdit.setPlaceholderText("Normal")
+        noEchoLineEdit.setPlaceholderText("NoEcho")
+        passwordLineEdit.setPlaceholderText("Password")
+        passwordEchoOnEditLineEdit.setPlaceholderText("PasswordEchoOnEdit")
+
+        normalLineEdit.setEchoMode(QLineEdit.Normal)        #设置回显模式
+        noEchoLineEdit.setEchoMode(QLineEdit.NoEcho)
+        passwordLineEdit.setEchoMode(QLineEdit.Password)
+        passwordEchoOnEditLineEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+
+        self.setLayout(formLayout)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QLineEditEchoMode()
+    main.show()
+    sys.exit(app.exec_())
--- 小窗口/窗口.py
+++ 小窗口/窗口.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -7,7 +8,23 @@ import 小窗口
 class my_Ui(小窗口.Ui_MainWindow):
     def __init__(self, MainWindow):
         super().setupUi(MainWindow)
+        self.random_number()
+        #self.sort_nunmber()
 
+
+    def random_number(self):
+        number = range(0, 100)
+        numbers = random.sample(number,10)
+        self.textBrowser.setText(str(numbers))
+        print("ok")
+
+    # def sort_number(self):
+    #     for i in range(len(numbers) - 1):
+    #         for j in range(len(nums) - i - 1):
+    #             if nums[j] > nums[j + 1]:
+    #                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+    #random_Button
 
 
 if __name__ == '__main__':
